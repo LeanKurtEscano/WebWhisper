0fdfa0057eb2dbb6840a14bdb2ed33f1bf14ce58
Lean Kurt <leankurtescano@gmail.com>
2025-08-31 01:59:28 +0800

    Setup router

--- frontend/page-whisper-extension/vite.config.ts
+++ frontend/page-whisper-extension/vite.config.ts
@@ -1,9 +1,5 @@
 
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './public/manifest.json'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -14,6 +10,5 @@ export default defineConfig({
   plugins: [
     react(), 
     tailwindcss(),
-    crx({ manifest })
   ],
 })
