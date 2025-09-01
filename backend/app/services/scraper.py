bdf99592e00624b02b9b8fad395628f93b32093f
Lean Kurt <leankurtescano@gmail.com>
2025-09-01 17:22:14 +0800

    Added Static Webpagre Scraper Class

--- backend/app/services/scraper.py
+++ backend/app/services/scraper.py
@@ -4,13 +4,26 @@ from bs4 import BeautifulSoup
 class WebScraper:
     def __init__(self, url:str):
         self.url = url
+        self.headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/115.0 Safari/537.36"
+    )
+}
         self.soup = self._get_soup()
     
-    
     def _get_soup(self):
-        response = requests.get(self.url, timeout=10)
-        response.raise_for_status() 
+        response = requests.get(self.url, headers=self.headers, timeout=10)
+        response.raise_for_status()
         return BeautifulSoup(response.text, "html.parser")
     
     def scrape(self) -> str:
-        return f"Scraped content from {self.url}"
+        tags_to_extract = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "article"]
+        extracted_content = []
+        
+        for tags in tags_to_extract:
+            elements = self.soup.find_all(tags)
+            for element in elements:
+                extracted_content.append(element.get_text(strip=True))
+        return "\n".join(extracted_content)
