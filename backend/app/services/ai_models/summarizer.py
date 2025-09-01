fdfdf49a6fbaab53cc130279899b834de6dac86e
Lean Kurt <leankurtescano@gmail.com>
2025-09-01 23:13:31 +0800

    Finished summarizer with gemini flash model

--- backend/app/services/ai_models/summarizer.py
+++ backend/app/services/ai_models/summarizer.py
@@ -1,15 +1,51 @@
 from concurrent.futures import ThreadPoolExecutor
+from google import genai
+
+from services.data.semantic_chunker import Chunking
+
+
+from concurrent.futures import ThreadPoolExecutor
 
 class Summarizer:
     
-    def __init__(self,max_workers=4):
-        self.model = ""
+    def __init__(self, max_workers=4):
+        self.model = genai.Client()
         self.max_workers = max_workers
+    
+    def summarize_chunks(self, text: str) -> str:
+        prompt = f"""
+        You are a helpful summarization assistant.
+        Please summarize the following text clearly,short and concisely,
+        focusing on the main points and ignoring redundancies:
         
-        
-    def summarize_chunk(self,text: str) -> str:
-        
-        summarize_text = []
+        {text}
+        """
+        response = self.model.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=prompt
+        )
+        return response.text
+    
+    def get_summary(self, text: str) -> str:
+     
+        chunks = Chunking.parallel_chunking(text)
+        texts = [c["text"] for c in chunks]
+
+        summarize_texts = []
         with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-            future = executor.submit()
-            return f"Summary of: {future.result()}"
+           
+            results = list(executor.map(self.summarize_chunks, texts))
+            summarize_texts.extend(results)
+            
+        final_prompt = f"""
+        You are an expert summarizer.
+        Combine the following partial summaries into one clear, 
+        coherent, and concise summary:
+
+        {" ".join(summarize_texts)}
+        """
+        response = self.model.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=final_prompt
+        )
+        return response.text
